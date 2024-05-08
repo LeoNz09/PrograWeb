978b35c6fbead892dbca7acafe4286979ff64bbd
LeoNz09 <160246035+LeoNz09@users.noreply.github.com>
2024-05-07 23:59:41 -0600

    Add files via upload

---
+++ FormularioComentariosPHP.php
@@ -0,0 +1,17 @@
+<?php
+
+if(isset($_POST['submit'])){
+	$para = "183782@upslp.edu.mx";
+	$asunto = "Enviando Información";
+	$nombre = $_POST['nombre'];
+	$correo = $_POST['email'];
+	$mensaje = $_POST['mensaje'];
+	$headers = "MIME-Version: 1.0\r\n";
+	$headers = "Content-type: text/html; charset=utf8\r\n";
+	$cuerpo = "Enviado por: $nombre\n E-mail: $correo\n Mensaje: $mensaje\n";
+
+	$bool = mail($para,$asunto,$cuerpo,$headers);
+} else {
+	echo "Ocurrio un error...";
+}
+?>
